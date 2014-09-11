66424a5b119e28761a094b7db0c67d22f64330ec
Hongchao Deng <hdeng@cloudera.com>
2014-09-10 20:55:56 -0700

    first

---
+++ src/main/java/com/mycompany/app/App.java
@@ -0,0 +1,14 @@
+package com.mycompany.app;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Hello world!
+ */
+public class App {
+  static final Logger LOG = LoggerFactory.getLogger(App.class);
+  public static void main(String[] args) {
+    LOG.info("Hello World!");
+  }
+}
---
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -0,0 +1,17 @@
+package com.mycompany.app;
+
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+public class AppTest {
+
+  @BeforeMethod
+  public void setUp() throws Exception {
+
+  }
+
+  @AfterMethod
+  public void tearDown() throws Exception {
+
+  }
+}
